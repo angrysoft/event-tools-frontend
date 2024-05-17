2f895f529dba57a24022aee92a969bac81bc5429
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-05-17 22:06:50 +0200

    table

--- src/app/workers-actions/workers/workers.component.ts
+++ src/app/workers-actions/workers/workers.component.ts
@@ -1,31 +1,37 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatTable, MatTableModule } from '@angular/material/table';
-import { WorkersDataSource } from './workers-datasource';
-import { WorkersItem } from '../../models/worker-item';
-import { WorkersService } from '../../services/workers.service';
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatTable, MatTableModule } from "@angular/material/table";
+import { WorkersDataSource } from "./workers-datasource";
+import { WorkersItem } from "../../models/worker-item";
+import { WorkersService } from "../../services/workers.service";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
-  selector: 'app-workers',
-  templateUrl: './workers.component.html',
-  styleUrl: './workers.component.scss',
+  selector: "app-workers",
+  templateUrl: "./workers.component.html",
+  styleUrl: "./workers.component.scss",
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule]
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class WorkersComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatTable) table!: MatTable<WorkersItem>;
   dataSource!: WorkersDataSource;
-  displayedColumns = ['id', 'firstName', "lastName"];
+  displayedColumns = ["id", "firstName", "lastName"];
 
-  constructor(private workerService: WorkersService) {
-    
-  }
+  constructor(private workerService: WorkersService) {}
 
   ngOnInit(): void {
-      this.dataSource = new WorkersDataSource(this.workerService);
+    this.dataSource = new WorkersDataSource(this.workerService);
   }
-
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
