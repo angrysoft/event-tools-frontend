445421ccc034084808588be87bfc73377d672540
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-05-22 21:13:21 +0200

    worker form

--- src/app/components/worker-form/worker-form.component.ts
+++ src/app/components/worker-form/worker-form.component.ts
@@ -1,11 +1,21 @@
-import { Component } from "@angular/core";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { RouterLink } from "@angular/router";
+import { RestResponse } from "../../models/rest-response";
 
 @Component({
   selector: "app-worker-form",
@@ -15,6 +25,9 @@ import { RouterLink } from "@angular/router";
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    FormsModule,
     ReactiveFormsModule,
     MatDividerModule,
     RouterLink,
@@ -22,11 +35,37 @@ import { RouterLink } from "@angular/router";
   templateUrl: "./worker-form.component.html",
   styleUrl: "./worker-form.component.scss",
 })
-export class WorkerFormComponent {
-  workerFrom = new FormGroup({
+export class WorkerFormComponent implements OnInit {
+  workerFrom = this.formBuilder.group({
     firstName: new FormControl(""),
     lastName: new FormControl(""),
-  })
+    phone: new FormControl(""),
+    email: new FormControl(""),
+    nickname: new FormControl(""),
+    color: new FormControl(""),
+    hasAppAccount: false,
+    user: new FormGroup({
+      username: new FormControl(""),
+      password: new FormControl(""),
+      password2: new FormControl(""),
+    }),
+  });
+
   formTitle: string = "Dodaj Pracownika";
+
+  hints = {
+    teams: [],
+    groups: [],
+    authorities : ["Koordynacja", "Technika", "Handel"]
+  }
+  constructor(private formBuilder: FormBuilder, private http:  HttpClient) {}
+
+
+  ngOnInit(): void {
+    this.http.get<RestResponse<WorkerHints>("/api/workers/hints").subscribe(
+      response => 
+    )
+  }
+
 
 }
---
+++ src/app/models/worker-hinst.ts
@@ -0,0 +1,19 @@
+export interface WorkerHints {
+  teams: Team[];
+  groups: Group[];
+  authorities: Authority[];
+}
+
+interface Team {
+  id: number;
+  name: string;
+}
+
+interface Group {
+  id: number;
+  name: string;
+}
+
+interface Authority {
+  authority: string;
+}
