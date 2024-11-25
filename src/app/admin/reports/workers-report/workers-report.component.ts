0f2b113c269d960a0af15256728cc035af163ad5
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-11-25 13:20:06 +0100

    export spinnelli

--- src/app/admin/reports/workers-report/workers-report.component.ts
+++ src/app/admin/reports/workers-report/workers-report.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LoaderComponent } from "../../../components/loader/loader.component";
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ActionToolbarComponent } from "../../../components/action-toolbar/action-toolbar.component";
 
 @Component({
   selector: 'app-workers-report',
-  imports: [],
+  imports: [LoaderComponent, MatExpansionModule, ActionToolbarComponent],
   templateUrl: './workers-report.component.html',
   styleUrl: './workers-report.component.scss'
 })
 export class WorkersReportComponent {
-
+  loading = signal<boolean>(false);
+  panelOpenState = signal<boolean>(true);
 }
