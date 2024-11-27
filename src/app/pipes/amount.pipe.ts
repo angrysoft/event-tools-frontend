a67231e69e5aad7ca79932b411ceb560d4a6757f
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-11-27 16:48:50 +0100

    space

--- src/app/pipes/amount.pipe.ts
+++ src/app/pipes/amount.pipe.ts
@@ -5,12 +5,12 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class AmountPipe implements PipeTransform {
   transform(value: string, hideAmount: boolean): unknown {
-    console.log(value, hideAmount);
     if (hideAmount) {
       return value
         .split("\n")
         .map((v) => v.split(":").at(0))
         .join("\n");
-    } else return value;
+    }
+    return value;
   }
 }
