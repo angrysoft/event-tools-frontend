0057443a12acc6ec3a17e0113a99daf86bb7b03a
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-12-09 10:00:08 +0100

    theme settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
 import { RouterOutlet } from "@angular/router";
 import { AuthService } from "./services/auth.service";
 
 @Component({
-    selector: "app-root",
-    imports: [RouterOutlet],
-    templateUrl: "./app.component.html",
-    styleUrl: "./app.component.scss"
+  selector: "app-root",
+  imports: [RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent implements OnInit {
   title = "Event Tools";
+  private readonly matIconRegistry = inject(MatIconRegistry);
+  private readonly auth = inject(AuthService);
 
-  constructor(
-    private readonly matIconRegistry: MatIconRegistry,
-    private readonly auth: AuthService,
-  ) {}
+  constructor() {
+    const r = document.querySelector(":root") as HTMLElement;
+    if (!r) return;
+    const theme = localStorage.getItem("theme");
+    if (!theme || theme === "system") {
+      r.style.setProperty("--color-theme", "light dark");
+    } else if (theme === "light") {
+      r.style.setProperty("--color-theme", "light");
+    } else {
+      r.style.setProperty("--color-theme", "dark");
+    }
+  }
 
   ngOnInit(): void {
     this.matIconRegistry.setDefaultFontSetClass("material-symbols-outlined");
