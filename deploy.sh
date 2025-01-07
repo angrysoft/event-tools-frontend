f40819939212965c01000939aa86dcdc3f1c55a7
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2025-01-07 08:13:55 +0100

    deploy to new server

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,7 @@ sha256sum event-tools-$VERSION.tgz > event-tools-$VERSION.tgz.sha
 scp event-tools-$VERSION.tgz nazgul:/home/uploads
 scp event-tools-$VERSION.tgz.sha nazgul:/home/uploads
 ssh nazgul "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
+
+scp event-tools-$VERSION.tgz et:/home/uploads
+scp event-tools-$VERSION.tgz.sha et:/home/uploads
+ssh et "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
