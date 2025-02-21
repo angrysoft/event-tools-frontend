c65fd5af66def59ea1ad83f130faf937c3e566c5
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2025-02-21 09:16:41 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,50 @@
 #!/bin/bash -e
 
-VERSION=$(npm pkg get version | sed 's/"//g')
-rm -frv dist
-ng build --configuration production
+build() {
+    echo "Building..."
+    VERSION=$(npm pkg get version | sed 's/"//g')
+    rm -frv dist
+    ng build --configuration production
 
-cd dist
+    cd dist
 
-tar czf event-tools-$VERSION.tgz event-tools
-sha256sum event-tools-$VERSION.tgz > event-tools-$VERSION.tgz.sha
+    tar czf event-tools-$VERSION.tgz event-tools
+    sha256sum event-tools-$VERSION.tgz > event-tools-$VERSION.tgz.sha
+}
 
-if [ "$1" = "prod" ]; then
-    echo "Deploying to production server"
-    scp event-tools-$VERSION.tgz et:/home/uploads
-    scp event-tools-$VERSION.tgz.sha et:/home/uploads
-    ssh et "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
-else
-    echo "Deploying to test server"
-    scp event-tools-$VERSION.tgz nazgul:/home/uploads
-    scp event-tools-$VERSION.tgz.sha nazgul:/home/uploads
-    ssh nazgul "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
-fi
+
+case "$1" in
+    prod)
+        echo "Deploying to production server"
+        build
+        scp event-tools-$VERSION.tgz et:/home/uploads
+        scp event-tools-$VERSION.tgz.sha et:/home/uploads
+        ssh et "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
+        ;;
+    test)
+        echo "Deploying to test server"
+        build
+        scp event-tools-$VERSION.tgz nazgul:/home/uploads
+        scp event-tools-$VERSION.tgz.sha nazgul:/home/uploads
+        ssh nazgul "ln -sf /home/uploads/event-tools-$VERSION.tgz /home/uploads/event-tools-latest.tgz;ln -sf /home/uploads/event-tools-$VERSION.tgz.sha /home/uploads/event-tools-latest.tgz.sha"
+        ;;
+    ver)
+        if [ "$2" = "patch" ]; then
+            npm version patch
+        elif [ "$2" = "minor" ]; then
+            npm version minor
+        elif [ "$2" = "major" ]; then
+            npm version major
+        else
+            echo "Usage: $0 ver {patch|minor|major}"
+            exit 1
+        fi
+        VERSION=$(npm pkg get version | sed 's/"//g')
+        sed -i "s/frontendVersion.=.\".*\"/frontendVersion = \"${VERSION}\"/g" /home/seba/workspace/event-tools-frontend/src/app/version.ts
+        git add src/app/version.ts
+        git commit -m "Bump version to $VERSION"
+        ;;
+    *)
+        echo "Usage: $0 {prod|test|ver}"
+        exit 1
+esac
